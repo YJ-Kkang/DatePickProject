e4c78031629827f10dfbc29d5112c3e6f76c2aef
neulbin <msg0447@gmail.com>
2025-02-01 17:06:29 +0900

    Fix: 엔티티 기본 틀 업로드 오류 재 커밋

--- src/main/java/jpabasic/datepickproject/common/entity/like/Like.java
+++ src/main/java/jpabasic/datepickproject/common/entity/like/Like.java
@@ -1,4 +1,38 @@
 package jpabasic.datepickproject.common.entity.like;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jpabasic.datepickproject.common.entity.post.Post;
+import jpabasic.datepickproject.common.entity.user.User;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "LIKE")
 public class Like {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(
+		name = "like_status",
+		nullable = false
+	)
+	private boolean likeStatus;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
 }
