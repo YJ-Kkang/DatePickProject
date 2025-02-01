2118e58defc820a8fe6bac9eee9cf812b5abdc7e
answerin1 <dabin990723@naver.com>
2025-02-02 02:42:32 +0900

    feat: responseDto에 토큰 주입 SignInUserResponseDto에 토큰 생성자 설정

--- src/main/java/jpabasic/datepickproject/dto/user/response/SignInUserResponseDto.java
+++ src/main/java/jpabasic/datepickproject/dto/user/response/SignInUserResponseDto.java
@@ -1,4 +1,14 @@
 package jpabasic.datepickproject.dto.user.response;
 
+import jpabasic.datepickproject.common.entity.user.User;
+import lombok.Getter;
+
+@Getter
 public class SignInUserResponseDto {
-}
+
+		private final String token;
+
+		public SignInUserResponseDto(String token) {
+			this.token = token;
+		}
+	}
