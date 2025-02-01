cacba6509d9bf24d575891e09a303f0a4d75dcf0
YJ-Kkang <kyujin995@gmail.com>
2025-02-01 21:11:47 +0900

    Comment: 유효성 관련 주석 추가

--- src/main/java/jpabasic/datepickproject/dto/post/request/CreatePostRequestDto.java
+++ src/main/java/jpabasic/datepickproject/dto/post/request/CreatePostRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class CreatePostRequestDto {
+	// todo @NotBlank, @Size는 제목과 내용에 대한 유효성 검사를 위해 추가한 사항들입니다. 추후 수정 및 삭제해도 무방합니다.
 	@NotBlank(message = "제목은 필수입니다.")
 	@Size(max = 100, message = "제목은 100자를 초과할 수 없습니다.")
 	private String title;
