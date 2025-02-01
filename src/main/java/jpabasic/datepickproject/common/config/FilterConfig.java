e22b6ba48bca49450fb1ae6cfbe4f7e0cb2368c8
answerin1 <dabin990723@naver.com>
2025-02-02 02:45:53 +0900

    feat: FilterConfig 기능 구현

--- src/main/java/jpabasic/datepickproject/common/config/FilterConfig.java
+++ src/main/java/jpabasic/datepickproject/common/config/FilterConfig.java
@@ -1,4 +1,30 @@
-package jpabasic.datepickproject.common.config;
+package jpabasic.datepickproject.config;
 
+import jpabasic.datepickproject.common.filter.JwtFilter;
+import jpabasic.datepickproject.common.utils.JwtUtil;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class FilterConfig {
+
+	private final JwtUtil jwtUtil;  // JwtUtil 주입
+
+	public FilterConfig(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+		FilterRegistrationBean<JwtFilter> registrationBean
+			= new FilterRegistrationBean<>(); // JwtFilter가 Spring bean으로 등록
+
+		registrationBean.setFilter(new JwtFilter(jwtUtil));  // JwtUtil을 필터에 주입
+
+		registrationBean.addUrlPatterns("/api/*");  // "api/"로 시작하는 모든 URL에 대해 필터를 적용
+		registrationBean.setOrder(1);  // 필터 우선순위 설정
+
+		return registrationBean;
+	}
 }
