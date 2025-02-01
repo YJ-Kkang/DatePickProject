3ff2127a94b947b199fb25d3e9dd35bd4ea36a66
YJ-Kkang <kyujin995@gmail.com>
2025-02-01 21:25:58 +0900

    Refactor: 커스텀 메서드를 제거, 기본 제공 메서드 사용(findById)

--- src/main/java/jpabasic/datepickproject/repository/post/PostRepositoryYJ.java
+++ src/main/java/jpabasic/datepickproject/repository/post/PostRepositoryYJ.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import jpabasic.datepickproject.common.entity.post.Post;
 
 public interface PostRepositoryYJ extends JpaRepository<Post, Long> {
-
-	Optional<Post> findPostById(Long postId);
 }
