c78b6a2415c177a9e21b1ab6de160d99e69af12d
KIMHAKSAN <bk351313@naver.com>
2025-02-03 15:20:02 +0900

    feat : COMMENT 생성, 수정, 조회, 삭제 추가 COMMENT 생성 기능 추가 COMMENT 수정 기능 추가 COMMENT 삭제 기능 추가(하드 -> 곧 소프트로 변경 예정) COMMENT 다건 조회 기능 추가

--- src/main/java/jpabasic/datepickproject/controller/comment/CommentController.java
+++ src/main/java/jpabasic/datepickproject/controller/comment/CommentController.java
@@ -1,12 +1,42 @@
 package jpabasic.datepickproject.controller.comment;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import jpabasic.datepickproject.dto.comment.request.CommentRequest;
+import jpabasic.datepickproject.dto.comment.request.CommentUpdateRequest;
+import jpabasic.datepickproject.dto.comment.response.CommentResponse;
+import jpabasic.datepickproject.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/comments")
 @RequiredArgsConstructor
 public class CommentController {
 
+	private final jpabasic.datepickproject.service.comment.CommentService commentService;
+
+	@PostMapping
+	public ResponseEntity<CommentResponse> createComment(@RequestBody CommentRequest request) {
+		return ResponseEntity.ok(commentService.createComment(request));
+	}
+
+	@GetMapping("/{postId}")
+	public ResponseEntity<List<CommentResponse>> getCommentsByPost(@PathVariable Long postId) {
+		return ResponseEntity.ok(commentService.getCommentsByPost(postId));
+	}
+
+	@PutMapping("/{commentId}")
+	public ResponseEntity<CommentResponse> updateComment(
+		@PathVariable Long commentId,
+		@RequestBody CommentUpdateRequest request) {
+		return ResponseEntity.ok(commentService.updateComment(commentId, request.getContent()));
+	}
+
+	@DeleteMapping("/{commentId}")
+	public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
+		commentService.deleteComment(commentId);
+		return ResponseEntity.noContent().build();
+	}
 }
