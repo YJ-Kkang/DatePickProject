f97627c0e0595343177e82530987ebb532c8b7e0
Jiwon <may0399@naver.com>
2025-02-04 19:35:58 +0900

    Feat: Post[update JWT 추가]

--- src/main/java/jpabasic/datepickproject/service/post/PostServiceJW.java
+++ src/main/java/jpabasic/datepickproject/service/post/PostServiceJW.java
@@ -54,18 +54,23 @@ public class PostServiceJW {
 
     @Transactional
     public UpdatePostResponseDto updatePost(Long postId, UpdatePostRequestDto updatePostRequestDto) {
+        // 토큰에서 유저 아이디 가져옴 -> 유저아이디로 유저 조회 -> 똑같?
+
+        // 토큰에서 추출한 유저아이디 가져오기
+        Long userId = updatePostRequestDto.getUserId();
 
         // 추출한 이메일로 유저를 조회 -> 없으면 예외 발생
-        User user = userRepository.findById(updatePostRequestDto.getUserId())
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다."));
 
         // 포스트를 조회 -> 없으면 예외 발생
         Post post = postRepositoryYJ.findById(postId)
                 .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다."));
 
+
         // 게시글 제목, 내용 수정 후 반환
         post.updatePost(updatePostRequestDto.getTitle(), updatePostRequestDto.getContent());
+
         return new UpdatePostResponseDto(post);
     }
 }
-
