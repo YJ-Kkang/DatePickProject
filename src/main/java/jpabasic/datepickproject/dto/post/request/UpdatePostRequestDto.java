2b568b68c8ab38114913f405d92b55af888a37cf
Jiwon <may0399@naver.com>
2025-02-04 12:44:58 +0900

    Fix: Post update 기능 수정

--- src/main/java/jpabasic/datepickproject/dto/post/request/UpdatePostRequestDto.java
+++ src/main/java/jpabasic/datepickproject/dto/post/request/UpdatePostRequestDto.java
@@ -1,10 +1,20 @@
 package jpabasic.datepickproject.dto.post.request;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
+@NoArgsConstructor
 public class UpdatePostRequestDto {
+    private Long userId;
     private String title;
     private String content;
+
+    public UpdatePostRequestDto(Long userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
 
 }
--- src/main/java/jpabasic/datepickproject/dto/post/response/UpdatePostResponseDto.java
+++ src/main/java/jpabasic/datepickproject/dto/post/response/UpdatePostResponseDto.java
@@ -8,12 +8,14 @@ import java.time.LocalDateTime;
 @Getter
 public class UpdatePostResponseDto {
     private final String message;
+    private final Long userId;
     private final String title;
     private final String content;
     private final LocalDateTime updatedAt;
 
     public UpdatePostResponseDto(Post post) {
         this.message = "게시글이 수정되었습니다.";
+        this.userId = post.getUser().getId();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.updatedAt = post.getUpdatedAt();
