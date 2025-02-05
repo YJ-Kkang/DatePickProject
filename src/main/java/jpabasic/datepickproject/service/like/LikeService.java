22e917f9b9a9bc40f2d69fb4645715269e80d792
neulbin <msg0447@gmail.com>
2025-02-05 15:13:15 +0900

    [Fix: 게시글 좋아요 상태 변경 버그 픽스]
    좋아요를 새롭게 생성 하는 로직에 이슈가 생겨서 새롭게 생성 하는 로직을 수정했습니다.

--- src/main/java/jpabasic/datepickproject/service/like/LikeService.java
+++ src/main/java/jpabasic/datepickproject/service/like/LikeService.java
@@ -8,12 +8,16 @@ import org.springframework.transaction.annotation.Transactional;
 import jpabasic.datepickproject.common.entity.like.Like;
 import jpabasic.datepickproject.common.entity.post.Post;
 import jpabasic.datepickproject.common.entity.user.User;
+import jpabasic.datepickproject.common.exception.CustomException;
+import jpabasic.datepickproject.common.exception.ErrorCode;
 import jpabasic.datepickproject.dto.like.LikeStatusResponseDto;
 import jpabasic.datepickproject.repository.like.LikeRepository;
 import jpabasic.datepickproject.repository.post.PostRepositoryYJ;
 import jpabasic.datepickproject.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class LikeService {
@@ -27,34 +31,36 @@ public class LikeService {
 	public LikeStatusResponseDto changeLikeStatus(Long postId, Long userId) {
 
 		User user = userRepository.findById(userId)
-			.orElseThrow(() -> new IllegalArgumentException("유저 정보를 찾을 수 없습니다."));
+			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 		Post post = postRepositoryYJ.findById(postId)
-			.orElseThrow(() -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-		// 기존의 좋아요 정보를 확인
+			.orElseThrow(() -> new CustomException(ErrorCode.POST_NOT_FOUND));
 
+		// 기존의 좋아요 정보를 확인
 		Optional<Like> optionalLike = likeRepository.findByPostIdAndUserId(postId, userId);
+
+		// 게시글에 좋아요를 처음 눌렀을 때 라이크를 새로 생성함.
+		Like like = optionalLike.orElseGet(() -> {
+			// 새로운 좋아요 데이터를 생성
+			Like newLike = new Like(user, post);
+			// 새롭게 생성된 좋아요 데이터를 레포지토리에 저장
+			likeRepository.save(newLike);
+			// 게시글의 좋아요 수를 증가 시킴.
+			post.changePostLike(post.getLikeCount() + 1);
+			return newLike;
+		});
 
 		// isPresent 는 값이 있을 때 true isEmpty 는 값이 없을 때
 		if (optionalLike.isPresent()) {
 			// 이미 좋아요가 있다면 -> 상태 변경 (좋아요 취소 혹은 다시 좋아요로 상태 변경이 가능함)
-			Like changeLike = optionalLike.get();
-			changeLike.switchLike();
-
+			like.switchLike();
 			/**
 			 * 좋아요 상태 변경에 따라 게시글 좋아요 개수 조정
 			 * likeStatus 의 상태가 true 면 ?뒤의 코드가 작동하고 false 면 : 뒤에 코드가 작동하는 방식의 삼항 연산자임.
 			 * 코드가 간결해서 좋지만 좀 풀어서 리팩토링 해보고 싶다
 			 */
-			post.changePostLike(changeLike.isLikeStatus() ? post.getLikeCount() + 1 : post.getLikeCount() - 1);
-		} else {
-			// 좋아요를 누른 적이 없었다면 새롭게 추가
-			Like newLike = new Like(user, post);
-			likeRepository.save(newLike);
-
-			// 좋아요 수 증가
-			post.changePostLike(post.getLikeCount() + 1);
+			post.changePostLike(like.isLikeStatus() ? post.getLikeCount() + 1 : post.getLikeCount() - 1);
 		}
 
-		return new LikeStatusResponseDto(optionalLike.get().checkLike(), post);
+		return new LikeStatusResponseDto(like.checkLike(), post);
 	}
 }
