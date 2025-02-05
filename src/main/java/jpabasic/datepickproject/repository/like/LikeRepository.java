1c189c083e922f87906a15b91609160ba0fb23e0
Jiwon <may0399@naver.com>
2025-02-05 16:32:03 +0900

    Feat: 좋아요 조회 기능 제거

--- src/main/java/jpabasic/datepickproject/repository/like/LikeRepository.java
+++ src/main/java/jpabasic/datepickproject/repository/like/LikeRepository.java
@@ -11,6 +11,4 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
 
 	Optional<Like> findByPostIdAndUserId(@Param("post_id") Long postId, @Param("userId") Long userId);
 
-	// 게시글의 좋아요 개수 조회 (status가 true인 경우만)
-	Long countByPostIdAndLikeStatusTrue(Long postId);
 }
